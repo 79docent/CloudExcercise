8789385ce9f3060d5f4f79851be33e1f75843fa3
mineralfiutta <s20574@pjwstk.edu.pl>
2023-01-10 17:38:04 +0100

    first  commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>About component.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Home component.</p>
+    </div>
+  );
+}
+
+export default About;
